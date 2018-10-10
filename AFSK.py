22aa4f150ac5eb07909d3f52c30b855fe32e23b1
killertux <bruno.canguru@hotmail.com>
2018-10-10 18:32:23 -0300

    Added the demodulator and audio recorder

--- AFSK.py
+++ AFSK.py
@@ -1,33 +1,102 @@
-# AFSK Modulation class
+# AFSK Modulation and Demodulation class
+import itertools
 
 import audiogen
 import math
 
+import numpy
+import scipy.signal.signaltools as signaltools
+import scipy.signal as signal
+from bitarray import bitarray
+from scipy.io.wavfile import read
 
 class AFSK:
     MARK_HZ = 1200.0
     SPACE_HZ = 2200.0
     BAUD_RATE = 1200.0
+    TWO_PI = 2.0 * math.pi
+    SAMPLE_RATE = audiogen.sampler.FRAME_RATE
 
     def encode(self, data):
-        for bit in data.tolist():
-            for samples in self.modulate(bit):
-                yield samples
+        data = itertools.chain(
+            bitarray("00000001"),
+            data,
+        )
+        for sample in itertools.chain(
+                audiogen.silence(0.15),
+                self.modulate(data),
+                audiogen.silence(0.15),
+        ):
+            yield sample * 0.5
 
-    def modulate(self, bit):
-        seconds_per_audio_sample = 1.0 / audiogen.sampler.FRAME_RATE
-        seconds_per_bit = 1.0 / self.BAUD_RATE
-        frequency = (self.MARK_HZ if bit else self.SPACE_HZ)
-        two_pi = 2 * math.pi
-        phase_change_per_sample = two_pi / (audiogen.sampler.FRAME_RATE / frequency)
+    def modulate(self, data):
+        seconds_per_sample = 1.0 / self.SAMPLE_RATE
+        phase, seconds= 0, 0
 
-        seconds, phase = 0, 0
+        clock = (x / self.BAUD_RATE for x in itertools.count(1))
+        tones = (self.MARK_HZ if bit else self.SPACE_HZ for bit in data)
 
-        while (seconds < seconds_per_bit):
-            yield math.sin(phase)
+        for boundary, frequency in itertools.izip(clock, tones):
+            phase_change_per_sample = self.TWO_PI / (self.SAMPLE_RATE / frequency)
 
-            seconds += seconds_per_audio_sample
-            phase += phase_change_per_sample
+            while seconds < boundary:
+                yield math.sin(phase)
 
-            if (phase > two_pi):
-                phase -= two_pi
+                seconds += seconds_per_sample
+                phase += phase_change_per_sample
+
+                if phase > self.TWO_PI:
+                    phase -= self.TWO_PI
+
+
+    def decode(self, file):
+        audio_input = numpy.array(read(file)[1], dtype=float)
+        filtered_signal = self.filterAFSK(audio_input)
+
+        bits = self.getBits(filtered_signal)
+        bits = self.removeStartSilence(bits)
+        return self.removeHeader(bits)
+
+    def removeStartSilence(self, bits):
+        silence = True
+        for bit in bits:
+            if bit == False:
+                silence = False
+            if silence == False:
+                yield bit
+
+    def removeHeader(self, bits):
+        header = True
+        for bit in bits:
+            if header == False:
+                yield bit
+            if bit:
+                header = False
+
+    def filterAFSK(self, audio_input):
+        differential = numpy.diff(audio_input, 1)
+        frequency_domain = numpy.abs(signaltools.hilbert(differential))
+        pir_filter = signal.firwin(numtaps=2, cutoff=1800, fs=self.SAMPLE_RATE)
+
+        return signal.filtfilt(pir_filter, [1.0], frequency_domain)
+
+    def getBits(self, filtered_signal):
+        bits_decoded = bitarray()
+
+        step = self.SAMPLE_RATE / self.BAUD_RATE
+
+        index = numpy.arange(step / 2.0, len(filtered_signal), step)
+        index = index.astype(int)
+        sampled_signal = filtered_signal[index]
+
+        mean = numpy.mean(numpy.abs(filtered_signal))
+
+        for bit in sampled_signal:
+            if bit > mean:
+                bits_decoded.append(False)
+            else:
+                bits_decoded.append(True)
+
+        return bits_decoded
+
+
--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 # Main python file
 import sys
 
-import argparse
-
 from AFSK import AFSK
+from AudioRecorder import AudioRecorder
 from bitarray import bitarray
+from time import sleep
 import audiogen
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(0o10, GPIO.OUT)
 
 
+if (sys.argv[1] == "modulate"):
+    with open (sys.argv[2], "rb") as f:
+        barray = bitarray()
+        barray.fromfile(f)
+        afsk = AFSK()
+        modulated_audio = afsk.encode(barray)
+        GPIO.output(0o10, True)
+        with open ("temp_wav.wav", "wb") as out:
+            audiogen.sampler.write_wav(out, modulated_audio)
+        
+        GPIO.output(0o10, False)
+        exit(0)
+#
+# with open (sys.argv[2], "rb") as f:
+#     afsk = AFSK()
+#     bits = bitarray(afsk.decode(f))
+#     print bits.tobytes()
 
-with open (sys.argv[1], "rb") as f:
-    barray = bitarray()
-    barray.fromfile(f)
-    afsk = AFSK()
-    modulated_audio = afsk.encode(barray)
-    audiogen.sampler.write_wav(sys.stdout, modulated_audio)
+recorder = AudioRecorder()
+recorder.listen()
